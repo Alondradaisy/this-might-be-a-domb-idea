2a4a67540948bd9faf1d767b5a9b3665b2fb94fe
Alondra Warren <daisy.codeimmersives.com>
2021-03-15 23:45:27 -0700

    step by step styling using JS

---
+++ main.js
@@ -0,0 +1,20 @@
+const element = document.querySelector('p');
+element.style.color = "lightblue";
+element.style.fontSize = "48px";
+
+const thirteenth = document.querySelector('ul');
+thirteenth.style.opacity = '0.5';
+
+const newParagraph = document.querySelector('p');
+newParagraph.innerHTML = 'I say, "Hi!"';
+
+let image = document.querySelector('img');
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+
+image.height = '300';
+
+// come back to this
+const image2 = document.querySelector('.img2');
+image2.src = 'https://www.wgecc.com/wp-content/uploads/2017/04/hummingbird.jpg';
+
+const li = document.querySelector('li')
