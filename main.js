440a21c7b134b73025c02dd8a86b440251ad8be5
Alondra Warren <daisy.codeimmersives.com>
2021-03-16 08:08:54 -0700

    add to li

--- main.js
+++ main.js
@@ -14,7 +14,11 @@ image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
 image.height = '300';
 
 // come back to this
-const image2 = document.querySelector('.img2');
-image2.src = 'https://www.wgecc.com/wp-content/uploads/2017/04/hummingbird.jpg';
+const image2 = document.querySelector('img');
+image2.src.innerHTML = 'https://www.wgecc.com/wp-content/uploads/2017/04/hummingbird.jpg';
+image2.height = '300';
 
-const li = document.querySelector('li')
+const li = document.querySelector('li');
+li.style.class = '.item';
+li.style.id = '#item-16';
+li.innerHTML = 'Won't get fooled again.'; 
